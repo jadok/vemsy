d0ab6568158f730c919904a655031e867117caae
Quentin Jadeau <quentin.jadeau@live.fr>
2018-07-18 22:26:08 +0200

    EOF + documentation

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/**
+ * Provide a JS endpoint to your TS application.
+ * @see {@link https://medium.com/@dupski/debug-typescript-in-vs-code-without-compiling-using-ts-node-9d1f4f9a94a} for further information.
+ */
+
 require('ts-node').register();
 // Now we can load and run ./my-typescript-code.ts...
 require('./core/index');
