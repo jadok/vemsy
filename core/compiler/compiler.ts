38a2bd8416599930dfe0f942ca0ed80435694b4a
jadok <quentin.jadeau@live.fr>
2018-09-19 23:10:30 +0200

    fix: Add files style compiler

--- core/compiler/compiler.ts
+++ core/compiler/compiler.ts
@@ -1,3 +1,5 @@
+import { join } from 'path'
+
 import { IStyleCompiler } from './style'
 
 import { SassStyle } from './sass/sassStyle'
@@ -15,5 +17,19 @@ export class Compiler {
 
   public setCompilers() {
     this.compiler.push(new SassStyle())
+  }
+
+  public compile(filename: string) {
+    const fileDirs = filename.split('/')
+    const srcTheme: string = join(this.themePath, ...fileDirs)
+    this.compiler
+      .reduce((acc: IStyleCompiler[], curr: IStyleCompiler) => {
+        if (curr.matchExtension(filename)) {
+          acc.push(curr)
+        }
+        return acc
+      }, [])
+      .forEach((compiler: IStyleCompiler) =>
+        compiler.compile(fileDirs, srcTheme, this.publicPath))
   }
 }
