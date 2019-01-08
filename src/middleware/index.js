7009dc2ff304860d85669e4393c369257b7eb115
jadok <quentin.jadeau@live.fr>
2019-01-08 02:57:43 +0100

    fix mocha test

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,11 +1,11 @@
 import data from './data.js'
 import markdown from './markdown.js'
 import render from './render.js'
-import routingFile from './routing-file.js'
+import { configureMiddleware } from './routing-file.js'
 
 export default {
   data,
   markdown,
   render,
-  routingFile: routingFile.configureMiddleware
+  routingFile: configureMiddleware
 }
