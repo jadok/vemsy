6d030e8ce657faa3e06f59399e9d9f38f8b5426e
jadok <quentin.jadeau@live.fr>
2019-02-21 09:41:06 +0100

    feat(route-include): update algo import pages
    BREAKING CHANGE
    
    - Page and template should now be implemented directly in the theme folder
      in a feature folder structure like.
    - The view, pages, template folder will soon be deleted

---
+++ src/utils/route-include.js
@@ -0,0 +1,28 @@
+
+const cycle = (data, currentPath, set) => {
+  const dataProperties = Object.keys(data)
+  dataProperties.forEach((dataKey) => {
+    const dataKeyPath = currentPath + '/' + dataKey
+    if (dataKey.endsWith('.page')) {
+      // set
+      data[dataKey].name = dataKeyPath
+      set.push(data[dataKey])
+    }
+    else {
+      cycle(data[dataKey], dataKeyPath, set)
+    }
+  })
+}
+
+/**
+ * 
+ * @param {Object} data
+ *   Json with instance object from 'include-all'
+ *
+ * @param {Array} set
+ *   set Data if validate is OK.
+ */
+const routeInclude = (data, set = []) =>
+  cycle(data, '', set)
+
+export default routeInclude
---
+++ test/fixtures/bigFeat/sample/sample.page.js
@@ -0,0 +1,5 @@
+import vemsy from '../../../../src/index.js'
+
+const TemplateDefault = new vemsy.type.Template('root')
+
+module.exports = new vemsy.type.Page('/', TemplateDefault)
