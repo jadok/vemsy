9a91be525c95603b5d1a76b18ec3df91cb99d2d8
jadok <quentin.jadeau@live.fr>
2019-02-21 21:50:34 +0100

    feat(theme): improve templating

--- src/middleware/render-theme.js
+++ src/middleware/render-theme.js
@@ -2,7 +2,10 @@ import { getAssetPathFromPage } from '../utils/path.js'
 
 const renderTemplateMiddleware = (req, res) => {
   const pageRendered = req.hidden_variables.pages[0]
-  const template = getAssetPathFromPage(pageRendered.name, pageRendered.template.filename + '.html.twig')
+  const template = getAssetPathFromPage(
+    pageRendered.name,
+    pageRendered.template.filename + pageRendered.template.extension
+  )
   res.render(template, req.variables)
 }
 
