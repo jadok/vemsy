3080c212e9fc55b6892a1b40fbb618b1547a6041
jadok <quentin.jadeau@live.fr>
2019-02-24 22:41:54 +0100

    fix(theme): template

--- src/utils/path.js
+++ src/utils/path.js
@@ -3,4 +3,7 @@ import { join } from 'path'
 const filePathToPath = (filePath) =>
   join(process.cwd(), ...filePath.split('/'))
 
+// get template path from page.
+const
+
 export default filePathToPath
