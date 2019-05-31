bb0fe475d72ab2497c54e649521142c7bc8f45b3
Quentin Jadeau <quentin.jadeau@live.fr>
2019-05-31 20:12:59 +0200

    Markdown (#40)
    * Style (#38)
    
    * feat: add sass compiler
    
    BREAKING CHANGE - page inclusion (see vemsy-cli)
    
    * rework: path implementation
    * docs: example folder moved to vemsy-cli
    * build: update dependencies
    
    * feat: add markdown-it
    
    * feat(markdown): implement markdown it
    
    * test: markdown update + nodejs engine
    
    * fix(security): dependencies

--- test/middlewares/markdown.spec.js
+++ test/middlewares/markdown.spec.js
@@ -2,6 +2,7 @@ import { expect } from 'chai'
 import 'mocha'
 import httpMocks from 'node-mocks-http'
 
+import markdownTask from '../../src/setup/markdown.js'
 import markdownMiddleware from '../../src/middleware/markdown.js'
 
 describe('middlewares - markdown', () => {
@@ -10,16 +11,20 @@ describe('middlewares - markdown', () => {
       method: 'GET',
       url: '/DevEnv',
     });
-    const response = httpMocks.createResponse()
+    global.__app = {}
+    var m = new markdownTask()
+    m.execute()
+      .then(() => {
+        const response = httpMocks.createResponse()
 
-    request.variables = {
-      data: ''
-    }
-    markdownMiddleware(request, response, () => null)
-    expect(typeof request.variables !== 'undefined').to.equal(true)
-    expect(typeof request.variables.data !== 'undefined').to.equal(true)
-    expect(request.variables.data).to.equal(request.variables.markdown)
-
+        request.variables = {
+          data: ''
+        }
+        markdownMiddleware(request, response, () => null)
+        expect(typeof request.variables !== 'undefined').to.equal(true)
+        expect(typeof request.variables.data !== 'undefined').to.equal(true)
+        expect(request.variables.data).to.equal(request.variables.markdown)
+      })
   })
 
   it('should transform markdown data', async () => {
@@ -28,15 +33,19 @@ describe('middlewares - markdown', () => {
       url: '/DevEnv',
     });
     const response = httpMocks.createResponse()
-
-    request.variables = {
-      data: 'Hello *World*!'
-    }
-    markdownMiddleware(request, response, () => null)
-    expect(typeof request.variables !== 'undefined').to.equal(true)
-    expect(typeof request.variables.data !== 'undefined').to.equal(true)
-    expect(request.variables.markdown).to.equal('<p>Hello <em>World</em>!</p>')
-  })
+    global.__app = {}
+    var m = new markdownTask()
+    m.execute()
+      .then(() => {
+        request.variables = {
+          data: 'Hello *World*!'
+        }
+        markdownMiddleware(request, response, () => null)
+        expect(typeof request.variables !== 'undefined').to.equal(true)
+        expect(typeof request.variables.data !== 'undefined').to.equal(true)
+        expect(request.variables.markdown).to.equal('<p>Hello <em>World</em>!</p>')
+      })
+    })
 
   it('should do nothing', async () => {
     const request  = httpMocks.createRequest({
